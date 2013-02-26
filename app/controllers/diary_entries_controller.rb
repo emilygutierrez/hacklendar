1ce29e88ee055b76cf1dfa7b401d1bbc029fb7db
Emily Ann Gutierrez <emilygutierrez86@gmail.com>
2013-02-25 21:29:46 -0500

    finishing up hacklendar

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -1,9 +1,17 @@
 class DiaryEntriesController < ApplicationController
 
 	def index
-		@day = params["day"].to_date
+		if params[:day]
+			@day = params[:day].to_date
+		else
+			@day = Date.today
+		end	
 		@diary_entries = DiaryEntry.where(created_at: @day.beginning_of_day..@day.end_of_day)
 		@all_entries = DiaryEntry.where(created_at: @day.beginning_of_month..@day.end_of_month)
+
+		@dates = @all_entries.map do |i|
+			i.created_at.to_date
+		end	
 
 		@diary_entries_positive = DiaryEntry.positive
 		
